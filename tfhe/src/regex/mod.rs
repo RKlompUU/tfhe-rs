af1a5481540baed59edb2335966b13f8d45cca3b
Rick Klomp <rick93nl@gmail.com>
2023-04-02 21:04:05 +0200

    remove example application from regex/mod.rs (it's in the docs now)

--- tfhe/src/regex/mod.rs
+++ tfhe/src/regex/mod.rs
@@ -2,19 +2,3 @@ pub mod ciphertext;
 pub mod engine;
 pub mod parser;
 pub mod execution;
-
-use crate::regex::ciphertext::{gen_keys, encrypt_str};
-use crate::regex::engine::has_match;
-
-// a simple example application
-pub(crate) fn main(content: &str, pattern: &str) {
-    let (client_key, server_key) = gen_keys();
-
-    println!("encrypting content..");
-    let ct_content = encrypt_str(&client_key, content);
-
-    println!("applying regex..");
-    let ct_res = has_match(&server_key, &ct_content.unwrap(), pattern).unwrap();
-    let res: u64 = client_key.decrypt(&ct_res);
-    println!("res: {:?}", res);
-}
